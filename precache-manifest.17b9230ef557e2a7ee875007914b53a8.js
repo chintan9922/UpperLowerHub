1938d50b987515e2da20c4c06aa440f7ec04763c
Chintan Chaudhari <69447439+chintan9922@users.noreply.github.com>
2024-03-14 13:09:26 +0530

    Updates

---
+++ precache-manifest.17b9230ef557e2a7ee875007914b53a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5119711d7b11d82af2a87c5503218d67",
+    "url": "/UpperLowerHub/index.html"
+  },
+  {
+    "revision": "5b35c9eea9d809ee5332",
+    "url": "/UpperLowerHub/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "bc853196e31e2d32a6c9",
+    "url": "/UpperLowerHub/static/js/2.a2b181a3.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/UpperLowerHub/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "5b35c9eea9d809ee5332",
+    "url": "/UpperLowerHub/static/js/main.f8b81829.chunk.js"
+  },
+  {
+    "revision": "20f91d95a70e34dc5632",
+    "url": "/UpperLowerHub/static/js/runtime~main.4e2ae1b5.js"
+  }
+]);
